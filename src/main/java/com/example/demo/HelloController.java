8e44b8926f6f061d01910e13be112ad5a0ca557b
tkyk1616 <taka28.tokyo@gmail.com>
2018-04-16 18:47:50 +0900

    Sprint

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "input your name :");//表示メッセージ
+		return mav;
+	}
+
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("name")String name,
+			ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "Hello " + name + " !");
+		mav.addObject("value", name);
+		return mav;
+	}
+}
---
+++ src/main/java/jp/co/realsys/model/StudentModel.java
@@ -0,0 +1,42 @@
+package jp.co.realsys.model;
+
+public class StudentModel {
+
+private String	id;
+private String	name;
+private String	birthDate;
+private String	address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
